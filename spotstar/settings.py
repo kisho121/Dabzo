824943417566d1bc3f9fa344f596c9448b262bf1
kisho121 <kishorehitter1995@gmail.com>
2025-12-07 22:51:32 +0530

    Reordered middleware for production

--- spotstar/settings.py
+++ spotstar/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -59,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 
     #'debug_toolbar.middleware.DebugToolbarMiddleware'
     
